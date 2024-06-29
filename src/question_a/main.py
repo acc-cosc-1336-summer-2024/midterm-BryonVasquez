0378728f6c9314eaacca22a6a30fa23b92366670
BryonVasquez <143046378+BryonVasquez@users.noreply.github.com>
2024-06-29 02:23:12 +0000

    Question 1 Mid Term

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,14 @@
-#add import
+#add import 
+def main(): 
+    while True: 
+        user_input = input("Enter a number (1-7) to get the corresponding day of the week (enter 'quite' to exit): ") 
+        if user_input.lower() == 'quit': 
+            print("Exiting the program....") 
+            break 
+        try: 
+            day_number = int(user_input) 
+            print (get_day_of_week(day_number)) 
+        except ValueError: 
+            print("Invalid input. Please enter a number or 'quit' to exit.")  
+
+    
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -2,11 +2,18 @@
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
+from src.question_a.question_a import get_day_of_week 
 
 class Test_Config(unittest.TestCase):
 
-    def test_question_a_config(self):
-        self.assertEqual(True, test_config())
+    def test_get_day_of_week(self): 
+        assert get_day_of_week(0) == "Invalid number" 
+        assert get_day_of_week(1) == "Monday" 
+        assert get_day_of_week(2) == "Tuesday" 
+        assert get_day_of_week(3) == "Wednesday" 
+        assert get_day_of_week(8) == "Invalid number"  
+
+        print ("All test cases passed") 
+
 
 
